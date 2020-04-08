c8c27bfa0f9e1367f637b167b1bef863d29bb2be
Zbigniew Downar <zbyszrom@Zbigniews-iMac.local>
2020-04-08 22:21:58 +0200

    navbar completed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,32 +2,103 @@ import React, {useState} from 'react'
 import { Link } from 'gatsby'
 import { FaAlignRight, FaFacebook, FaTwitter, FaYoutube } from 'react-icons/fa'
 import logo from "../images/logo.svg"
+import styles from "./navbar.module.css"
+import styled from 'styled-components'
+
+   const Nav = styled.nav`
+    @media (min-width: 992px) {
+        max-width: 1170px;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
+   `   
+   const Sheader = styled.div `
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 1.25rem;
+   `
+   const Button = styled.button`
+    background: transparent;
+    border: none;
+    outline: none;
+    &:hover {
+      cursor: pointer
+    }
+    @media (min-width: 992px) {
+        display: none;
+    }
+   `
+   const SFaAlignRight = styled(FaAlignRight)`
+   color: var(--primary);
+    font-size: 1.5rem
+`
+const SLink = styled(Link)`
+      padding: 1rem 1.25rem;
+      text-decoration: none;
+      text-transform: capitalize;
+      color: var(--mainBlack);
+      transition: var(--mainTransition);
+      font-weight: bold;
+      letter-spacing: var(--mainSpacing);
+      &:hover {
+        color: var(--primaryColor);
+      }
+`
+const Social = styled.div`
+        display: none;
+        @media (min-width: 992px){
+        display: flex;
+      
+      a {
+        color: var(--primary);
+        margin: 0 0.5rem;
+        font-size: 1.2rem;
+        transition: var(--transition);
+        &:hover {
+          color: var(--black);
+          transform: translateY(-5px);
+        }
+      }
+    }
+`
+
+
+
 const Navbar = ()=>{
 
-    const [isOpen, setNav] = useState()
+    const [isOpen, setNav] = useState(false)
     const toggleNav = ()=>{
         setNav(isOpen => !isOpen)
     }
 
     return(
-        <nav>
+    <Nav>
+        
+          <Sheader>
            <img src = {logo} alt = ''/>
-           <button type="button" onClick={toggleNav}>
-           <FaAlignRight/>
-           </button>
-            <ul style = {isOpen ? {height: '216px'} : {height: 0}}>
-             <li><Link to = "/">home</Link></li>     
-             <li><Link to = "/tours">Tours</Link></li>     
-             <li><Link to = "/blog">Blog</Link></li>    
-             <li><Link to = "/contact">Contact</Link></li>               
-            </ul>
-           <div>
+           <Button type="button"  onClick={toggleNav}>
+           <SFaAlignRight />
+           </Button>
+          </Sheader>
+        <ul className={
+          isOpen
+          ? `${styles.links} ${styles.showNav}`
+          : `${styles.links}`
+          }>
+             <li><SLink to = "/">home</SLink></li>     
+             <li><SLink to = "/tours">Tours</SLink></li>     
+             <li><SLink to = "/contact">Contact</SLink></li>               
+        </ul>
+        <Social>
             <a href="" target="_blank" rel="noopener noreferrer"><FaFacebook size = '1.8rem' /></a>
             <a href="" target="_blank" rel="noopener noreferrer"><FaTwitter size = '1.8rem'/></a>
             <a href="" target="_blank" rel="noopener noreferrer"><FaYoutube size = '1.8rem'/></a>
-           </div>
-
-        </nav>
+        </Social>
+        
+    </Nav>
     )
 }
 
