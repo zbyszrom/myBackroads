82b756c7370e11312354e871dddbff9b2c1ba15f
Zbigniew Downar <zbyszrom@Zbigniews-iMac.local>
2020-05-07 15:38:27 +0200

    netlify identity

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,62 +1,75 @@
-import React from 'react'
-import {Link} from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 import { FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa"
-import styled from 'styled-components'
+import styled from "styled-components"
 
 const Ul = styled.ul`
-    a {
-  display: inline-block;
-  text-transform: uppercase;
-  color: white;
-  margin: 0.5rem 1rem;
-  letter-spacing: var(--mainSpacing);
-  transition: var(--transition);
-  font-weight: bold;
-  &:hover {
+  a {
+    display: inline-block;
+    text-transform: uppercase;
     color: white;
-  }
-    }
-` 
-const CRight = styled.div`
-    text-transform: capitalize;
+    margin: 0.5rem 1rem;
     letter-spacing: var(--mainSpacing);
-    line-height: 2;
+    transition: var(--transition);
+    font-weight: bold;
+    &:hover {
+      color: white;
+    }
+  }
+`
+const CRight = styled.div`
+  text-transform: capitalize;
+  letter-spacing: var(--mainSpacing);
+  line-height: 2;
 `
 const Icons = styled.div`
-    a{
-        display: inline-block;
-        margin: 1rem;
-        font-size: 1.3rem;
-        color: var(--primary);
-        transition: var(--mainTransition);
-        &:hover {
-            color: white;
-  }
+  a {
+    display: inline-block;
+    margin: 1rem;
+    font-size: 1.3rem;
+    color: var(--primary);
+    transition: var(--mainTransition);
+    &:hover {
+      color: white;
     }
+  }
 `
 
-const Footer = ()=>{
-    return(
-        <footer>
-            
-            <Ul>
-             <li><Link to = "/">home</Link></li>     
-             <li><Link to = "/tours">Tours</Link></li>     
-             <li><Link to = "/blog">Blog</Link></li>    
-             <li><Link to = "/contact">Contact</Link></li>               
-            </Ul>
-           
-           <Icons>
-            <a href="" target="_blank" rel="noopener noreferrer"><FaFacebook size = '1.8rem' /></a>
-            <a href="" target="_blank" rel="noopener noreferrer"><FaTwitter size = '1.8rem'/></a>
-            <a href="" target="_blank" rel="noopener noreferrer"><FaYoutube size = '1.8rem'/></a>
-           </Icons>
-           
-            <CRight>copyright &copy; zbyszrom {new Date().getFullYear()} all
-            rights reserved</CRight>
-           
-        </footer>
-    )
+const Footer = () => {
+  return (
+    <footer>
+      <Ul>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+          <Link to="/tours">Tours</Link>
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </Ul>
+
+      <Icons>
+        <a href="facebook.com" target="_blank" rel="noopener noreferrer">
+          <FaFacebook size="1.8rem" />
+        </a>
+        <a href="twitter.com" target="_blank" rel="noopener noreferrer">
+          <FaTwitter size="1.8rem" />
+        </a>
+        <a href="youtube.com" target="_blank" rel="noopener noreferrer">
+          <FaYoutube size="1.8rem" />
+        </a>
+      </Icons>
+
+      <CRight>
+        copyright &copy; zbyszrom {new Date().getFullYear()} all rights reserved
+      </CRight>
+    </footer>
+  )
 }
 
 export default Footer
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,105 +1,115 @@
-import React, {useState} from 'react'
-import { Link } from 'gatsby'
-import { FaAlignRight, FaFacebook, FaTwitter, FaYoutube } from 'react-icons/fa'
+import React, { useState, useEffect } from "react"
+import { Link } from "gatsby"
+import { FaAlignRight, FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa"
 import logo from "../images/logo.svg"
-import styled from 'styled-components'
+import styled from "styled-components"
+import netlifyIdentity from "netlify-identity-widget"
 
-   const Nav = styled.nav`
-    @media (min-width: 960px) {
-        max-width: 1200px;
-        margin: 0 auto;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-      }
-   `   
-   const Sheader = styled.div `
+const Nav = styled.nav`
+  @media (min-width: 960px) {
+    max-width: 1200px;
+    margin: 0 auto;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 1rem 1.25rem;
-   `
-   const Button = styled.button`
-    background: transparent;
-    border: none;
-    outline: none;
-    &:hover {
-      cursor: pointer
-    }
-    @media (min-width: 960px) {
-        display: none;
-    }
-   `
-   const SFaAlignRight = styled(FaAlignRight)`
-   color: var(--primary);
-    font-size: 1.5rem
+  }
+`
+const Sheader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 1.25rem;
+`
+const Button = styled.button`
+  background: transparent;
+  border: none;
+  outline: none;
+  &:hover {
+    cursor: pointer;
+  }
+  @media (min-width: 960px) {
+    display: none;
+  }
+`
+const SFaAlignRight = styled(FaAlignRight)`
+  color: var(--primary);
+  font-size: 1.5rem;
 `
 const SLink = styled(Link)`
-      padding: 1rem 1.25rem;
-      text-transform: capitalize;
-      color: var(--mainBlack);
-      transition: var(--mainTransition);
-      font-weight: bold;
-      letter-spacing: var(--mainSpacing);
-      &:hover {
-        color: var(--primaryColor);
-      }
+  padding: 1rem 1.25rem;
+  text-transform: capitalize;
+  color: var(--black);
+  transition: var(--transition);
+  font-weight: bold;
+  letter-spacing: var(--mainSpacing);
+  &:hover {
+    color: var(--primary);
+    transform: translateY(-5px);
+  }
 `
+
 const Social = styled.div`
-        display: none;
-        @media (min-width: 960px){
-        display: flex;
-      
-      a {
-        color: var(--primary);
-        margin: 0 0.5rem;
-        font-size: 1.2rem;
-        transition: var(--transition);
-        &:hover {
-          color: var(--black);
-          transform: translateY(-5px);
-        }
+  display: none;
+  @media (min-width: 960px) {
+    display: flex;
+
+    a {
+      color: var(--primary);
+      margin: 0 0.5rem;
+      font-size: 1.2rem;
+      transition: var(--transition);
+      &:hover {
+        color: var(--black);
+        transform: translateY(-5px);
       }
     }
+  }
 `
 
+const Navbar = () => {
+  const [isOpen, setNav] = useState(false)
+  const toggleNav = () => {
+    setNav(isOpen => !isOpen)
+  }
+  useEffect(() => {
+    netlifyIdentity.init()
+  }, [])
 
-
-const Navbar = ()=>{
-
-    const [isOpen, setNav] = useState(false)
-    const toggleNav = ()=>{
-        setNav(isOpen => !isOpen)
-    }
-
-    return(
+  return (
     <Nav>
-        
-          <Sheader>
-           <img src = {logo} alt = ''/>
-           <Button type="button"  onClick={toggleNav}>
-           <SFaAlignRight />
-           </Button>
-          </Sheader>
+      <Sheader>
+        <img src={logo} alt="" />
+        <Button type="button" onClick={toggleNav}>
+          <SFaAlignRight />
+        </Button>
+      </Sheader>
 
-        <ul className={
-          isOpen
-          ? 'links showNav' 
-          : `links`
-          }>
-             <li><SLink to = "/">home</SLink></li>     
-             <li><SLink to = "/tours">Tours</SLink></li>     
-             <li><SLink to = "/contact">Contact</SLink></li>               
-        </ul>
+      <ul className={isOpen ? "links showNav" : `links`}>
+        <li>
+          <SLink to="/">home</SLink>
+        </li>
+        <li>
+          <SLink to="/tours">Tours</SLink>
+        </li>
+        <li>
+          <SLink to="/contact">Contact</SLink>
+        </li>
+      </ul>
+      <div data-netlify-identity-menu></div>
 
-        <Social>
-            <a href="" target="_blank" rel="noopener noreferrer"><FaFacebook size = '1.8rem' /></a>
-            <a href="" target="_blank" rel="noopener noreferrer"><FaTwitter size = '1.8rem'/></a>
-            <a href="" target="_blank" rel="noopener noreferrer"><FaYoutube size = '1.8rem'/></a>
-        </Social>
-        
+      <Social>
+        <a href="facebook.com" target="_blank" rel="noopener noreferrer">
+          <FaFacebook size="1.8rem" />
+        </a>
+        <a href="twitter.com" target="_blank" rel="noopener noreferrer">
+          <FaTwitter size="1.8rem" />
+        </a>
+        <a href="youtube.com" target="_blank" rel="noopener noreferrer">
+          <FaYoutube size="1.8rem" />
+        </a>
+      </Social>
     </Nav>
-    )
+  )
 }
 
 export default Navbar
